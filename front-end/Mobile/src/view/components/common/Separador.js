e82ca14070031a0c8814cfb165a4bccdc7284d5f
juanArvol <arboledaperdomo@gmail.com>
2026-05-08 16:11:34 -0500

    delete folder

--- front-end/Mobile/src/view/components/common/Separador.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { View } from 'react-native';
-import stylescommon from './style/Style';
-
-const Separador = () => (
-  <View style={stylescommon.separador} />
-);
-
-export default Separador;
